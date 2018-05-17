93031ff876c2422402200151a8503ad3a3392235
larissamoreira <larissamgusmao@gmail.com>
2018-05-16 22:03:31 -0300

    add static

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+const cepinfo = document.querySelector('#id_cep')
+const logradouro = document.querySelector('#id_logradouro')
+const bairro = document.querySelector('#id_bairro')
+const cidade = document.querySelector('#id_cidade')
+const estado = document.querySelector('#id_estado')
+
+cepinfo.addEventListener('blur', function() {
+    let cep = cepinfo.value 
+    let url = `https://viacep.com.br/ws/${cep}/json/`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            logradouro.value = data.logradouro
+            bairro.value = data.bairro
+            estado.value = data.uf
+            cidade.value = data.localidade
+        })
+})
